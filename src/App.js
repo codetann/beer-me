40de489c13c3348f23be85db3db6bdd8a9d24d4d
Tanner <thetannerthomas@gmail.com>
2021-10-23 12:35:36 -0600

    add location hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { Search } from "./components";
 import { VStack } from "@chakra-ui/react";
+import { useLocation } from "./hooks";
 
 function App() {
   const [search, setSearch] = useState("");
+  const { location, getLocation } = useLocation();
 
   return (
     <VStack w="100%" minH="100vh" maxH="100vh">
       <Search
         value={search}
         setValue={setSearch}
+        getLocation={getLocation}
         onChange={(e) => setSearch(e.target.value)}
       />
     </VStack>
