9d17f05cc49fdfb3d36f525120ae1a6620a99e38
codetann <thetannerthomas@gmail.com>
2021-10-23 23:52:59 -0600

    add localstorage hook

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,28 +1,21 @@
 /**
  * <Map />
- *
- * TODO [error]: Map component is not working properly.
- * -  It is not rendering the map properly.
- * -  Only shows up some of the map when it does.
- * -  Importing leaflet css makes the map not render at all.
  */
 
 import React from "react";
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { VStack } from "@chakra-ui/layout";
 
 export const Map = ({ position }) => {
   if (!position) return <p>Loading...</p>;
   return (
     <MapContainer
-      center={position}
       zoom={13}
+      center={position}
       style={{ height: "100vh", width: "100vw" }}
     >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <Marker position={position}>
         <Popup>
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+// create a hook to use localStorage
+import { useRef, useState, useEffect } from "react";
+
+function useLocalStorage(key, initialValue) {
+  // get the initial value
+  const initialValueRef = useRef(initialValue);
+  // set the initial value
+  initialValueRef.current = initialValue;
+
+  // useState to get value
+  const [value, setValue] = useState(() => {
+    // get from localStorage
+    const item = window.localStorage.getItem(key);
+    // return the value or the initial value
+    return item ? JSON.parse(item) : initialValueRef.current;
+  });
+
+  // useEffect to store value in localStorage
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
